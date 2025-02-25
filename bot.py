ce72fcf6c70701c00697d23e6555a1cc024b4d5a
nikanovo <145991428+nikanovo@users.noreply.github.com>
2025-02-25 22:31:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,17 @@ from telebot import types
 import requests
 from bs4 import BeautifulSoup as b
 import cloudscraper
-   
 
+bot = telebot.TeleBot('8078422610:AAHI-iMDiiXgBQKsWpt5jHmSFvj2yR_H_oE')  # Замените на ваш токен
 
-bot = telebot.TeleBot('8078422610:AAHI-iMDiiXgBQKsWpt5jHmSFvj2yR_H_oE')
-
-scraper = cloudscraper.create_scraper()
-r = scraper.get(url, timeout=10)
+def get_inf(url):
+    scraper = cloudscraper.create_scraper()
+    try:
+        r = scraper.get(url, timeout=10)
+        r.raise_for_status()  # Проверка на ошибки HTTP
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при запросе: {e}")
+        return []
 
     soup = b(r.text, 'html.parser')
 
@@ -29,12 +33,12 @@ r = scraper.get(url, timeout=10)
         head = news_heads[j].strip()
         text = str(links[j][0]).split('"')[1]
         ans.append([date, head, text])
+    
     return ans[::-1]
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
     bot.send_message(message.chat.id, 'Привет! Я бот спортивных новостей. \nИспользуйте команду */sport* для просмотра последних *спортивных* событий.', parse_mode="Markdown")
-
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
@@ -42,33 +46,26 @@ def callback(call):
         if call.data == 'q_1':
             URL = 'https://m.sport-express.ru/futsal/news/?isEditorialChoice=1'
             inf = get_inf(URL)
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text=f'{inf[0][0]} \n{inf[0][1]} \n[Читать далее]({inf[0][2]})', parse_mode="Markdown")
-            inf = inf[1:]
-            for l in inf:
-                bot.send_message(call.message.chat.id, f'{l[0]} \n{l[1]} \n[Читать далее]({l[2]})', parse_mode="Markdown")
+            if inf:
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                      text=f'{inf[0][0]} \n{inf[0][1]} \n[Читать далее]({inf[0][2]})', parse_mode="Markdown")
+                inf = inf[1:]
+                for l in inf:
+                    bot.send_message(call.message.chat.id, f'{l[0]} \n{l[1]} \n[Читать далее]({l[2]})', parse_mode="Markdown")
         elif call.data == 'q_2':
             URL = 'https://www.sport-express.ru/hockey/news/?isEditorialChoice=1'
             inf = get_inf(URL)
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text=f'{inf[0][0]} \n{inf[0][1]} \n[Читать далее]({inf[0][2]})', parse_mode="Markdown")
-            inf = inf[1:]
-            for l in inf:
-                bot.send_message(call.message.chat.id, f'{l[0]} \n{l[1]} \n[Читать далее]({l[2]})', parse_mode="Markdown")
+            if inf:
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                      text=f'{inf[0][0]} \n{inf[0][1]} \n[Читать далее]({inf[0][2]})', parse_mode="Markdown")
+                inf = inf[1:]
+                for l in inf:
+                    bot.send_message(call.message.chat.id, f'{l[0]} \n{l[1]} \n[Читать далее]({l[2]})', parse_mode="Markdown")
         elif call.data == 'q_3':
             URL = 'https://www.sport-express.ru/basketball/news/?isEditorialChoice=1'
             inf = get_inf(URL)
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text=f'{inf[0][0]} \n{inf[0][1]} \n[Читать далее]({inf[0][2]})', parse_mode="Markdown")
-            inf = inf[1:]
-            for l in inf:
-                bot.send_message(call.message.chat.id, f'{l[0]} \n{l[1]} \n[Читать далее]({l[2]})', parse_mode="Markdown")
-
-@bot.message_handler(commands=['sport'])
-def sport_command(message):
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    item = types.InlineKeyboardButton('Последние новости Футзала', callback_data='q_1')
-    item2 = types.InlineKeyboardButton('Последние новости Хоккея', callback_data='q_2')
-    item3 = types.InlineKeyboardButton('Последние новости Баскетбола', callback_data='q_3')
-    markup.add(item, item2, item3)
-
-    bot.send_message(message.chat.id, 'Вы можете выбрать:', reply_markup=markup)
-
-bot.infinity_polling()
+            if inf:
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                      text=f'{inf[0][0]} \n{inf[0][1]} \n[Читать далее]({inf[0][2]})', parse_mode="Markdown")
+                inf = inf[1:]
+                for l in inf:
